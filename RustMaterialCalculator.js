4af32c2a3b59f8c781e75b8a5547d2561103290f
Albin Uddenbjörk <au222fh@student.lnu.se>
2022-09-21 12:32:29 +0200

    Add support for doorway calculation.

--- RustMaterialCalculator.js
+++ RustMaterialCalculator.js
@@ -1,10 +1,31 @@
 export class RustMaterialCalculator {
-  static test () {
-    return "t"
+  static WallCalculator (buildingTier, Amount, materials) {
+    materials[0] += 50 * Amount
+    if (buildingTier == 1) {
+      materials[0] += 200 * Amount
+    } else if (buildingTier == 2) {
+      materials[1] += 300 * Amount
+    } else if (buildingTier == 3) {
+      materials[2] += 200 * Amount
+    } else if (buildingTier == 4) {
+      materials[3] += 25 * Amount
+    }
+
+    return materials
   }
-  static WallCalculator (AmountOfWalls, tier) {
-    const materials = ['0', '0', '0', '0']
-    materials[0] = 25 * AmountOfWalls
-    return "t"
+
+  static DoorwayCalculator (buildingTier, Amount, materials) {
+    materials[0] += 35 * Amount
+    if (buildingTier == 1) {
+      materials[0] += 140 * Amount
+    } else if (buildingTier == 2) {
+      materials[1] += 210 * Amount
+    } else if (buildingTier == 3) {
+      materials[2] += 140 * Amount
+    } else if (buildingTier == 4) {
+      materials[3] += 18 * Amount
+    }
+
+    return materials
   }
 }
--- TestApp.js
+++ TestApp.js
@@ -4,17 +4,36 @@ import { stdin as input, stdout as output } from 'node:process';
 
 const scanner = readline.createInterface({ input, output });
 let done = false
+const materials = [0, 0, 0, 0]
 
 while (done === false) {
-let buildingType = await scanner.question('What would you like to calculate? 1. Wall 2. Foundation 3. Floor 4. Triangle Foundation 5. Triangle FLoor 6. Doorway 7. Window ')
-let buildingAmount = await scanner.question('How many will you need? ')
+  let buildingType = await scanner.question('What would you like to calculate? 1. Wall 2. Foundation 3. Floor 4. Triangle Foundation 5. Triangle FLoor 6. Doorway 7. Window ')
+  buildingType = parseInt(buildingType)
 
-let answer = await scanner.question('Are you done? y/n ')
-if (answer = 'y') {
-  done = true
-}
+  let buildingTier = await scanner.question('What material? 1. Wood 2. Stone 3. Metal 4. High Quality Metal ')
+  buildingTier = parseInt(buildingTier)
 
-console.log(RustMaterialCalculator.test)
+  let buildingAmount = await scanner.question('How many will you need? ')
+  buildingAmount = parseInt(buildingAmount)
+
+  calculateBuilding(buildingType, buildingAmount, buildingTier, materials)
+  let answer = await scanner.question('Are you done? y/n ')
+  if (answer = 'y') {
+    done = true
+  }
+
+  console.log(materials)
 }
 
 scanner.close();
+
+function calculateBuilding (buildingType, buildingAmount, buildingTier, materials) {
+  console.log('smh')
+  if (buildingType === 1) {
+    console.log('t')
+    RustMaterialCalculator.WallCalculator(buildingTier, buildingAmount, materials)
+  } else if (buildingType === 7) {
+    RustMaterialCalculator.DoorwayCalculator(buildingTier, buildingAmount, materials)
+  }
+
+}
