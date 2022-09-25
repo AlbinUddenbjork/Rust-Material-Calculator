5366d764f6f702d42e7d881875553a5883766ca1
Albin Uddenbjörk <au222fh@student.lnu.se>
2022-09-25 14:37:43 +0200

    Add support for square floors and other minor fixes.

--- RustMaterialCalculator.js
+++ RustMaterialCalculator.js
@@ -1,21 +1,49 @@
 export class RustMaterialCalculator {
   static WallCalculator (buildingTier, Amount, materials) {
-    materials[0] += 50 * Amount
-    if (buildingTier == 1) {
-      materials[0] += 200 * Amount
-    } else if (buildingTier == 2) {
-      materials[1] += 300 * Amount
-    } else if (buildingTier == 3) {
-      materials[2] += 200 * Amount
-    } else if (buildingTier == 4) {
-      materials[3] += 25 * Amount
+    if (materials === undefined) {
+      const materials = [0, 0, 0, 0]
     }
+
+    materials[0] += 50 * Amount
+    
+    materials = this.FoundationWallSupport (buildingTier, Amount, materials)
 
     return materials
   }
 
+  static SquareFoundationCalculator (buildingTier, Amount, materials) {
+    if (materials === undefined) {
+      const materials = [0, 0, 0, 0]
+    }
+
+    materials[0] += 50 * Amount
+    
+    materials = this.FoundationWallSupport (buildingTier, Amount, materials)
+
+    return materials
+  }
+
+  static SquareFloorCalculator (buildingTier, Amount, materials) {
+    if (materials === undefined) {
+      const materials = [0, 0, 0, 0]
+    }
+
+    materials[0] += 25 * Amount
+    
+    if (buildingTier == 1) {
+      materials[0] += 100 * Amount
+    } else if (buildingTier == 2) {
+      materials[1] += 150 * Amount
+    } else if (buildingTier == 3) {
+      materials[2] += 100 * Amount
+    } else if (buildingTier == 4) {
+      materials[3] += 13 * Amount
+    }
+  }
+
   static DoorwayCalculator (buildingTier, Amount, materials) {
     materials[0] += 35 * Amount
+    
     if (buildingTier == 1) {
       materials[0] += 140 * Amount
     } else if (buildingTier == 2) {
@@ -24,6 +52,20 @@ export class RustMaterialCalculator {
       materials[2] += 140 * Amount
     } else if (buildingTier == 4) {
       materials[3] += 18 * Amount
+    }
+
+    return materials
+  }
+
+  static FoundationWallSupport (buildingTier, Amount, materials) {
+    if (buildingTier == 1) {
+      materials[0] += 200 * Amount
+    } else if (buildingTier == 2) {
+      materials[1] += 300 * Amount
+    } else if (buildingTier == 3) {
+      materials[2] += 200 * Amount
+    } else if (buildingTier == 4) {
+      materials[3] += 25 * Amount
     }
 
     return materials
--- TestApp.js
+++ TestApp.js
@@ -7,7 +7,7 @@ let done = false
 const materials = [0, 0, 0, 0]
 
 while (done === false) {
-  let buildingType = await scanner.question('What would you like to calculate? 1. Wall 2. Foundation 3. Floor 4. Triangle Foundation 5. Triangle FLoor 6. Doorway 7. Window ')
+  let buildingType = await scanner.question('What would you like to calculate? 1. Wall 2. Square foundation 3. Square floor 4. Triangle foundation 5. Triangle floor 6. Doorway 7. Window ')
   buildingType = parseInt(buildingType)
 
   let buildingTier = await scanner.question('What material? 1. Wood 2. Stone 3. Metal 4. High Quality Metal ')
@@ -32,6 +32,10 @@ function calculateBuilding (buildingType, buildingAmount, buildingTier, material
   if (buildingType === 1) {
     console.log('t')
     RustMaterialCalculator.WallCalculator(buildingTier, buildingAmount, materials)
+  } else if (buildingType === 2) {
+    RustMaterialCalculator.SquareFoundationCalculator(buildingTier, buildingAmount, materials)
+  } else if (buildingType === 3) {
+    RustMaterialCalculator.SquareFloorCalculator(buildingTier, buildingAmount, materials)
   } else if (buildingType === 7) {
     RustMaterialCalculator.DoorwayCalculator(buildingTier, buildingAmount, materials)
   }
